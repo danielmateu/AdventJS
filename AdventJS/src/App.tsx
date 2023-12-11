2c2899b5c2370430c9e505bad814abce9d5a8255
Daniel Mateu <danielmateu86@gmail.com>
2023-12-11 10:01:29 +0100

    ex10 added

--- AdventJS/src/App.tsx
+++ AdventJS/src/App.tsx
@@ -1,38 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { drawGift } from './exercices/07-drawGift'
-import { organizeGifts } from './exercices/08-organizeGifts'
+import { adjustLights } from './exercices/09-adjustLights'
+// import { drawGift } from './exercices/07-drawGift'
+// import { organizeGifts } from './exercices/08-organizeGifts'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  drawGift(4, '+');
-  const orgGifts = organizeGifts(`76a11b`);
-  console.log(orgGifts);
+
+  // drawGift(4, '+');
+  // const orgGifts = organizeGifts(`76a11b`);
+  // console.log(orgGifts);
+  const lights = ['🟢', '🔴', '🟢', ];
+
+  console.log(adjustLights(lights));
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
